42e74f5d8fc2adb9e56036003af157489c38d020
nasirulla <rehmanansirulla1124@gmail.com>
2026-06-05 03:14:53 +0530

    Change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import Route from "./services/RouteApp";
 import useCurrentUser from "./hooks/useCurrentUser";
 
-
-export const ServerURL = "https://gen-kwj5.onrender.com";
+export const ServerURL = import.meta.env.VITE_SERVER_URL || "https://gen-kwj5.onrender.com";
 
 const App = () => {
   useCurrentUser();
